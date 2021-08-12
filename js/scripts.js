31f3232923862e1dd18bbf3c7957a28c745f8558
Becket Harvey <becketfc@gmail.com>
2021-08-12 12:23:20 -0700

    functional voting site

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,9 @@
-// business logic
-
-function Age() {
-  const voter = parseInt(prompt("Please enter your age."));
-  if (voter >= 18) {
-    $('#learntovote').show();
-  } else {
-    $('#cantvote').show();
-  }
-}
-
-
-
-// user logic
-$(document).ready(function () {
-  $("form#").submit(function (event) {
-    event.preventDefault();
+$(document).ready(function() {
+    const voter = parseInt(prompt("What is your age?"));
+    if (voter >= 18) {
+      $('#learntovote').show();
+    } else {
+      $('#cantvote').show();
+    }
   });
-});
-
-
 
